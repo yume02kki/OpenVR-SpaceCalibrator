a46fc38f8abadf8aa2faa46f299169b618c84f0d
ben <beneshel3@gmail.com>
2026-04-21 15:14:50 +0300

    shared libs port

--- OpenVR-SpaceCalibratorDriver/OpenVR-SpaceCalibratorDriver.h
+++ OpenVR-SpaceCalibratorDriver/OpenVR-SpaceCalibratorDriver.h
@@ -1,7 +1,3 @@
 #pragma once
 
-#ifdef OPENVRSPACECALIBRATORDRIVER_EXPORTS
-#define OPENVRSPACECALIBRATORDRIVER_API extern "C" __declspec(dllexport)
-#else
-#define OPENVRSPACECALIBRATORDRIVER_API extern "C" __declspec(dllimport)
-#endif
+#define OPENVRSPACECALIBRATORDRIVER_API extern "C" __attribute__((visibility("default")))
